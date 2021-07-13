76c44caf7ae87a3dfaef2854a4a95f7c8449f8d4
Marshall Johnson <marshalljohnson193@gmail.com>
2021-07-12 20:22:59 -0600

    cycling through phases

--- App.js
+++ App.js
@@ -13,6 +13,9 @@ export default function App() {
   const [blueCp, setBlueCP] = useState(0)
   //*Phase
   const [phase, setPhase] = useState(0)
+  //*Battle 
+  const [battleRound, setBattleRound] = useState(1)
+  const [playerTurn, setPlayerTurn] = useState(undefined)
 
   const handleScore = (player, value) => {
     if (player === 'red') {
@@ -23,22 +26,58 @@ export default function App() {
     }
   }
 
+  const handleCP = (player, value) => {
+    if (player === 'red') {
+      value === '+' ? setRedCP(redCp + 1) : redCp > 0 ? setRedCP(redCp - 1) : null
+    }
+    if (player === 'blue') {
+      value === '+' ? setBlueCP(blueCp + 1) : blueCp > 0 ? setBlueCP(blueCp - 1) : null
+    }
+  }
+
+  const handlePhase = () => {
+    phase === phases.length - 1 ? setPhase(0) : setPhase(phase + 1)
+  }
+
   return (
-    <SafeAreaView style={styles.row}>
-      <View style={styles.col, styles.red}>
-        <Text >{redPoints}</Text>
-        <View style={styles.row}>
-          <Button onPress={() => handleScore('red', '+')} title="+" />
-          <Button onPress={() => handleScore('red', '-')} title="-" />
-        </View>
+    <SafeAreaView style={styles.col}>
+
+      <View>
+        <Text>Battle Round {battleRound}</Text>
+        <Text>{phases[phase]} Phase </Text>
+        <Button onPress={() => handlePhase()} title='Next Phase' />
       </View>
-      <View style={styles.col, styles.blue}>
-        <Text>{bluePoints}</Text>
-        <View style={styles.row}>
-          <Button onPress={() => handleScore('blue', '+')} title="+" />
-          <Button onPress={() => handleScore('blue', '-')} title="-" />
+
+      <SafeAreaView style={styles.points}>
+        <View style={styles.col, styles.red}>
+          <Text>Red Player's Victory Points</Text>
+          <Text >{redPoints}</Text>
+          <View style={styles.row}>
+            <Button onPress={() => handleScore('red', '+')} title="+" />
+            <Button onPress={() => handleScore('red', '-')} title="-" />
+          </View>
+          <Text>Red Player's Command Points </Text>
+          <Text>{redCp}</Text>
+          <View style={styles.row}>
+            <Button onPress={() => handleCP('red', '+')} title="+" />
+            <Button onPress={() => handleCP('red', '-')} title="-" />
+          </View>
         </View>
-      </View>
+        <View style={styles.col, styles.blue}>
+          <Text>Blue Player's Victory Points</Text>
+          <Text>{bluePoints}</Text>
+          <View style={styles.row}>
+            <Button onPress={() => handleScore('blue', '+')} title="+" />
+            <Button onPress={() => handleScore('blue', '-')} title="-" />
+          </View>
+          <Text>Blue Player's Command Points </Text>
+          <Text>{blueCp}</Text>
+          <View style={styles.row}>
+            <Button onPress={() => handleCP('blue', '+')} title="+" />
+            <Button onPress={() => handleCP('blue', '-')} title="-" />
+          </View>
+        </View>
+      </SafeAreaView>
 
     </SafeAreaView>
   );
@@ -50,7 +89,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     backgroundColor: '#fff',
     justifyContent: 'center',
-
+  },
+  points: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    width: '98%'
   },
   col: {
     flex: 1,
@@ -58,8 +103,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
-    margin: 50
-
   },
   blue: {
     backgroundColor: 'blue',
